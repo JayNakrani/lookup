a58b492f81fbbf15e4946e312191e6b8e83fb1bf
Dhananjay Nakrani <dhananjaynakrani@gmail.com>
2017-12-31 10:27:08 +0530

    Refactor common code and add bloomberg.

--- lookup.js
+++ lookup.js
@@ -1,36 +1,22 @@
-var chrunchbase = chrome.contextMenus.create({
-  "title": "Chrunchbase: '%s'",
-  "contexts": ['selection'],
-  "onclick": function(info, tab) {
-    chrome.tabs.create({url: googleUrl(info.selectionText + ' Crunchbase')});
-  },
-});
+var items = [
+  'Chrunchbase',
+  'AngelList',
+  'LinkedIn',
+  'Bloomberg',
+  'Meaning',
+]
 
-var angelList = chrome.contextMenus.create({
-  "title": "Angel List: '%s'",
-  "contexts": ['selection'],
-  "onclick": function(info, tab) {
-    chrome.tabs.create({url: googleUrl(info.selectionText + ' AngelList')});
-  },
-});
-
-var linkedin = chrome.contextMenus.create({
-  "title": "LinkedIn: '%s'",
-  "contexts": ['selection'],
-  "onclick": function(info, tab) {
-    chrome.tabs.create({url: googleUrl(info.selectionText + ' LinkedIn')});
-  },
-});
-
-// English is not my native language! :(
-var meaning = chrome.contextMenus.create({
-  "title": "Meaning of '%s'",
-  "contexts": ['selection'],
-  "onclick": function(info, tab) {
-    chrome.tabs.create({url: googleUrl(info.selectionText + ' meaning')});
-  },
-});
+for(i = 0; i < items.length; i++) {
+  let item = items[i];  // `let` to create a avoid closure capture.
+  chrome.contextMenus.create({
+    "title": item + ": '%s'",
+    "contexts": ['selection'],
+    "onclick": function(info, tab) {
+      chrome.tabs.create({url: googleUrl(info.selectionText + ' ' + item)});
+    },
+  });
+}
 
 function googleUrl(query) {
-	return 'https://google.com/#q=' + encodeURIComponent(query);
+  return 'https://google.com/#q=' + encodeURIComponent(query);
 }
